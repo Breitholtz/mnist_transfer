d9a007751982aff94ccdca7db2adbf453be19661
Adam Breitholtz <adambre@alvis1.int.private>
2021-09-30 16:03:59 +0200

    add module script

---
+++ load_modules.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+ml GCC/8.3.0
+ml CUDA/10.1.243
+ml OpenMPI/3.1.4
+ml IPython
+ml scikit-learn
+ml fosscuda/2019b 
+ml Horovod/0.19.1-TensorFlow-2.1.0-Python-3.7.4
+ml scikit-learn/0.21.3-Python-3.7.4
+ml Keras/2.3.1-Python-3.7.4
